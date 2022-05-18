cd4bfce6ba33ef38f5977d3afd810e072f6b7707
Lamu-ctrl <59745093+Lamu-ctrl@users.noreply.github.com>
2022-05-18 22:22:46 +0800

    Update 452. Minimum Number of Arrows to Burst Balloons.cpp

--- 452. Minimum Number of Arrows to Burst Balloons.cpp
+++ 452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         int n=points.size();
-        int res=0;
+        int res=1;
         if(n==0) return 0;
         sort(points.begin(),points.end());    
-        int left=points[0][0] , right=points[0][1];      
+        int  right=points[0][1];      
         for(int i=1;i<n;i++){
             if(right >= points[i][0]){
                 right=min(right,points[i][1]);
-                left=max(left,points[i][0]);
             }
             else{
-                left=points[i][0];
                 right=points[i][1];
                 res++;
             }
         }
-        return res+1;
+        return res;
     }
 };
+
+/*
+Constraints:
+
+1 <= points.length <= 105
+points[i].length == 2
+-231 <= xstart < xend <= 231 - 1
+*/
