3f68cb684df01d0895ce84615321899ff3fb7644
Lamu-ctrl <59745093+Lamu-ctrl@users.noreply.github.com>
2022-05-21 23:47:18 +0800

    Create 11. Container With Most Water.cpp

---
+++ 11. Container With Most Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+        int res=0;
+        while(right>=left){
+            res=max(res, (right-left)*min(height[right],height[left]));
+            if(height[right]>height[left]){
+                res=max(res, (right-left)*height[left] );
+                left++;
+            }
+            else{
+                res=max(res, (right-left)*height[right] );
+                right--;                
+            }
+        }
+        return res;
+    }
+};
