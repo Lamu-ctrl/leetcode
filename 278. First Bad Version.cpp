98d6d9411c29cc0a963def0a7606ff66a35cccfb
Lamu-ctrl <59745093+Lamu-ctrl@users.noreply.github.com>
2022-06-04 03:50:49 +0800

    Create 278. First Bad Version.cpp

---
+++ 278. First Bad Version.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=0;
+        int right=n;
+        int mid;
+        while(right>left){
+            mid=left+(right-left)/2;
+            if( isBadVersion(mid) ){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return right;
+    }
+};
